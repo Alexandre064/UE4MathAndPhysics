4e405f112f7c8dcd61e4034da8d63f9e67a1fff4
Alex <alex.cabanne@gmail.com>
2019-10-16 16:56:37 +0300

    project_unity

---
+++ physics_test_2/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    GameObject prefab;
+    int dragForce=0, massForce=1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        prefab = Resources.Load("Sphere") as GameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject projectile = Instantiate(prefab) as GameObject;
+            Rigidbody rigidbody = projectile.GetComponent<Rigidbody>();
+            rigidbody.velocity = Camera.main.transform.forward * 40;
+            rigidbody.drag = dragForce;
+            rigidbody.mass = massForce;
+            projectile.transform.position = transform.position + Camera.main.transform.forward * 2;
+
+        }
+        if (Input.GetKeyUp(KeyCode.C))
+        {
+            dragForce++;
+        }
+        if (Input.GetKeyUp(KeyCode.V))
+        {
+            dragForce--;
+        }
+        if (Input.GetKeyUp(KeyCode.B))
+        {
+            massForce++;
+        }
+        if (Input.GetKeyUp(KeyCode.N))
+        {
+            massForce--;
+        }
+    }
+}
